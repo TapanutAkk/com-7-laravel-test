81fc9e17bf171e6289fa99d9bb221c3fad62c992
Tapanut Akkarasubdacho <tapanut.akk@gmail.com>
2022-11-20 00:20:06 +0700

    create contact table

---
+++ database/migrations/2022_11_19_144116_create_contacts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50);
+            $table->string('email', 50);
+            $table->string('phone', 10);
+            $table->string('message', 500);
+            $table->timestamp('start_date');
+            $table->timestamp('end_date');
+            $table->boolean('is_need_on_site_service')->default(false);
+            $table->string('address', 500)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contacts');
+    }
+};
